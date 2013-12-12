94e3b79cbd6bb075511e70e5720a20b69245656b
Sudhir Nimavat <sudhir_nimavat@yahoo.com>
2013-12-12 19:33:52 +0530

    Bump plugin version to 0.7

--- NimbleGrailsPlugin.groovy
+++ NimbleGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.springframework.mail.javamail.JavaMailSenderImpl
 
 class NimbleGrailsPlugin {
-	def version = "0.6"
+	def version = "0.7"
 	def grailsVersion = "2.0.1 > *"
 	def title = "Nimble"
 	def author = "Sudhir Nimavat"
