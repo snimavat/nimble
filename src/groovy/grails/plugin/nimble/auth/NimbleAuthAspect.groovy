6f5ccad574a3c7bf465b4caefdddd26cbe09e027
Rohit Kumbhar <rohit.kumbhar@gmail.com>
2014-05-22 00:44:39 -0700

    Added SPEL support to permissions to parse the params map
    Permissions can now be specified using a SPEL expression.
    For example: @NimbleAuth(role = 'USER', permission = 'entity:action:#{params[\'id\']}')
    This expression will result in checking the permission
    entity:action:12345 if params.id for the controller is 12345

--- src/groovy/grails/plugin/nimble/auth/NimbleAuthAspect.groovy
+++ src/groovy/grails/plugin/nimble/auth/NimbleAuthAspect.groovy
@@ -8,6 +8,12 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
 import org.aspectj.lang.reflect.MethodSignature
+import org.springframework.expression.EvaluationContext
+import org.springframework.expression.Expression
+import org.springframework.expression.ParserContext
+import org.springframework.expression.common.TemplateParserContext
+import org.springframework.expression.spel.standard.SpelExpressionParser
+import org.springframework.expression.spel.support.StandardEvaluationContext
 
 import java.lang.reflect.Method
 
@@ -15,6 +21,8 @@ import java.lang.reflect.Method
 public class NimbleAuthAspect {
 
     private static final Logger log = Logger.getLogger(NimbleAuthAspect.class)
+    public static final SpelExpressionParser spelExpressionParser = new SpelExpressionParser()
+    public static final ParserContext parserContext = new TemplateParserContext()
 
     @Pointcut("@annotation(grails.plugin.nimble.auth.NimbleAuth)")
     public void doNimbleAuth() {}
@@ -23,6 +31,7 @@ public class NimbleAuthAspect {
     public Object executeAuth(ProceedingJoinPoint pjp) throws Throwable {
 
         def target = pjp.getTarget()
+
         MethodSignature methodSignature = pjp.getSignature();
         Method method = methodSignature.method;
         NimbleAuth authPref = method.getAnnotation(NimbleAuth.class)
@@ -37,78 +46,96 @@ public class NimbleAuthAspect {
         if (target.metaClass.respondsTo(target, "getAuthenticatedSubject")) {
 
             Subject subject = target.getAuthenticatedSubject()
-            def subjectAuthenticated = subject.authenticated
-
-            if (log.isDebugEnabled()) {
-                log.debug("Is subject authenticated = ${subjectAuthenticated}")
-            }
-
             def request = target.request
+
+            // Check authentication flag
+            def subjectAuthenticated = subject.authenticated
+            log.debug("Is subject authenticated = ${subjectAuthenticated}")
+
+            // Re-direct to login page
             if (!subjectAuthenticated) {
-
-                log.info("Subject not authenticated. Re-directing to login page...")
-
-                def targetUri = new StringBuilder(request.forwardURI[request.contextPath.size()..-1])
-                def query = request.queryString
-                if (query) {
-                    if (!query.startsWith('?')) {
-                        targetUri << '?'
-                    }
-                    targetUri << query
-                }
-
-
-                def redirectUri = target.grailsApplication.config.security.shiro.redirect.uri
-                if (redirectUri) {
-                    target.redirect(uri: redirectUri + "?targetUri=${targetUri.encodeAsURL()}")
-                } else {
-                    target.redirect(
-                            controller: "auth",
-                            action: "login",
-                            params: [targetUri: targetUri.toString()])
-                }
+                redirectToLoginPage(request, target)
             } else {
 
-                if (log.isDebugEnabled()) {
-                    log.debug("Subject is authenticated")
-                }
+                log.debug("Subject is authenticated")
 
                 // Check role if specified
                 if (StringUtils.isNotEmpty(role)) {
-
-                    if (log.isDebugEnabled()) {
-                        log.debug("Checking subject ${subject.principal} for role ${role} ");
-                    }
-
-                    boolean hasRequestedRole = subject.hasRole(role)
-
-                    if (!hasRequestedRole) {
-                        log.info("Subject ${subject.principal} does not have role ${role} ");
-                        target.redirect(
-                                controller: "auth",
-                                action: "unauthorized")
-                    }
+                    checkSubjectRole(subject, role, target)
                 }
 
+                // Check permission if specified
                 if (StringUtils.isNotEmpty(permission)) {
-
-                    if (log.isDebugEnabled()) {
-                        log.debug("Checking subject ${subject.principal} for permission ${permission} ");
-                    }
-
-                    boolean hasPermission = subject.isPermitted(permission)
-
-                    if (!hasPermission) {
-                        log.info("Subject ${subject.principal} does not have permission ${permission} ");
-                        target.redirect(
-                                controller: "auth",
-                                action: "unauthorized")
-                    }
+                    evaluatePermission(permission, target, subject)
                 }
             }
         }
 
         return pjp.proceed();
+    }
+
+    private void evaluatePermission(String permission, target, Subject subject) {
+        Expression expression = spelExpressionParser.parseExpression(permission, parserContext);
+        EvaluationContext ectx = new StandardEvaluationContext(target)
+        ectx.setVariable("params", target.params)
+        Object value = expression.getValue(ectx)
+
+        if (log.isDebugEnabled()) {
+            log.debug("Checking subject ${subject.principal} for permission ${value} ");
+        }
+
+        boolean hasPermission = subject.isPermitted(value.toString())
+
+        if (!hasPermission) {
+            log.info("Subject ${subject.principal} does not have permission ${value} ");
+            target.redirect(
+                    controller: "auth",
+                    action: "unauthorized")
+        }
+    }
+
+    private void checkSubjectRole(Subject subject, role, target) {
+        if (log.isDebugEnabled()) {
+            log.debug("Checking subject ${subject.principal} for role ${role} ");
+        }
+
+        boolean hasRequestedRole = subject.hasRole(role)
+
+        if (!hasRequestedRole) {
+            log.info("Subject ${subject.principal} does not have role ${role} ");
+            target.redirect(
+                    controller: "auth",
+                    action: "unauthorized")
+        }
+    }
+
+    /**
+     * Re-direct to login page if required
+     * @param request Incoming HTTP request
+     * @param target Controller, service or filter executing this operation
+     */
+    private void redirectToLoginPage(request, target) {
+
+        log.info("Subject not authenticated. Re-directing to login page...")
+
+        def targetUri = new StringBuilder(request.forwardURI[request.contextPath.size()..-1])
+        def query = request.queryString
+        if (query) {
+            if (!query.startsWith('?')) {
+                targetUri << '?'
+            }
+            targetUri << query
+        }
+
+        def redirectUri = target.grailsApplication.config.security.shiro.redirect.uri
+        if (redirectUri) {
+            target.redirect(uri: redirectUri + "?targetUri=${targetUri.encodeAsURL()}")
+        } else {
+            target.redirect(
+                    controller: "auth",
+                    action: "login",
+                    params: [targetUri: targetUri.toString()])
+        }
     }
 }
 
